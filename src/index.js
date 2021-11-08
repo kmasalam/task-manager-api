b571794cc7339851f8faea70587d21849b412478
kmasalam <mccyber007@gmail.com>
2021-11-08 09:33:00 +0600

    trying to fix cors problem

--- src/index.js
+++ src/index.js
@@ -13,9 +13,10 @@ const port = process.env.PORT;
 
 
 
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
 
